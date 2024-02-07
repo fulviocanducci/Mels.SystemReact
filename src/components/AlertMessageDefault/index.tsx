10a071b737c0f2d0efa884895ecd8278e2b95565
fulviocanducci <fulviocanducci@hotmail.com>
2024-02-07 16:05:22 -0300

    AlertMessageDefault update color

--- src/components/AlertMessageDefault/index.tsx
+++ src/components/AlertMessageDefault/index.tsx
@@ -7,7 +7,7 @@ type AlertMessageDefaultType = {
 
 export default function AlertMessageDefault({ title, body }: AlertMessageDefaultType) {
   return (
-    <Alert key={"success"} variant={"light"}>
+    <Alert key={"success"} variant={"light"} className="border border-success text-success">
       <Alert.Heading>{title}</Alert.Heading>
       {body}
     </Alert>
